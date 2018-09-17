6de06c20e419041b8e65f29c7239685b7952b663
Ning Yu <nyu@pivotal.io>
2018-09-17 16:09:38 +0800

    Update extension gp_distribution_policy.
    Update extension gp_distribution_policy to adapt the new numsegments
    column in catalog gp_distribution_policy.
    
    NOTE: this updated version can only work with gpdb 6, we need to figure
    out a way to auto detect the existance of the numsegments column.

--- contrib/gp_distribution_policy/uninstall_gp_distribution_policy.sql
+++ contrib/gp_distribution_policy/uninstall_gp_distribution_policy.sql
@@ -2,4 +2,4 @@ SET search_path = public;
 
 DROP FUNCTION gp_heap_distribution_check(text);
 
-DROP FUNCTION gp_distribution_policy_heap_table_check(oid, smallint[]);
+DROP FUNCTION gp_distribution_policy_heap_table_check(oid, smallint[], integer);
